25a343492d95b3b8c6edec56d57295f5f1ea48fb
vitoraquino <vitoraquino80@gmail.com>
2022-11-21 22:58:18 -0300

    02.Thymeleaf e Bootstrap

---
+++ src/main/java/br/com/mvc/mudi/controller/HomeController.java
@@ -0,0 +1,29 @@
+package br.com.mvc.mudi.controller;
+
+import br.com.mvc.mudi.model.Pedido;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @GetMapping("/home")
+    public String home(Model model) {
+
+        Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+        List<Pedido> pedidos = query.getResultList();
+
+        model.addAttribute("pedidos", pedidos);
+        return "home";
+    }
+}
