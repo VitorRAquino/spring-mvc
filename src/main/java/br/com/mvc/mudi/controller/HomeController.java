a078340c8aa00901d127a9dbce048b4acf56e346
vitoraquino <vitoraquino80@gmail.com>
2022-11-21 23:23:04 -0300

    03.Integração com Spring Data

--- src/main/java/br/com/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/mvc/mudi/controller/HomeController.java
@@ -1,28 +1,21 @@
 package br.com.mvc.mudi.controller;
 
 import br.com.mvc.mudi.model.Pedido;
+import br.com.mvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
+    @Autowired
+    private PedidoRepository pedidoRepository;
     @GetMapping("/home")
     public String home(Model model) {
-
-        Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-        List<Pedido> pedidos = query.getResultList();
-
+        List<Pedido> pedidos = pedidoRepository.findAll();
         model.addAttribute("pedidos", pedidos);
         return "home";
     }
