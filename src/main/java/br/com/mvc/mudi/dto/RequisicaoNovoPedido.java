4025e39c7c672c6df0a93e19f313292575bb836e
vitoraquino <vitoraquino80@gmail.com>
2022-11-22 23:10:27 -0300

    05.Implementando Bean Validation

--- src/main/java/br/com/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -2,11 +2,17 @@ package br.com.mvc.mudi.dto;
 
 import br.com.mvc.mudi.model.Pedido;
 
+import javax.validation.constraints.NotBlank;
+
 public class RequisicaoNovoPedido {
 
+    @NotBlank(message = "O campo do produto é obrigatório.")
     private String nomeProduto;
+    @NotBlank(message = "O campo da URL do produto é obrigatório.")
     private String urlDoProduto;
+    @NotBlank(message = "O campo da URL da imagem é obrigatório.")
     private String urlDaImagem;
+    @NotBlank
     private String descricao;
 
     public String getNomeProduto() {
